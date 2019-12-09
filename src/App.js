849909282d8344f0d83120e05af6ee3cad247cbc
sushmitha-b-s <bssushmitha26@gmail.com>
2019-12-09 18:29:39 +0100

    implemented scss for web-page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import './App.css';
+import './styles/App.scss';
 import store from './store'
 import { Provider } from 'react-redux'
 import { Route } from 'react-router-dom'
--- src/components/Jokes.js
+++ src/components/Jokes.js
@@ -14,14 +14,16 @@ export default class Jokes extends Component {
 
     render() {
         return (
-            <div>
+            <div className="flexCentre">
                 <h1>Programming Joke</h1>
-                {!this.props.joke && ".."}
+                <div className="container">
+                    
+                    {!this.props.joke && ".."}
 
-                <p>{this.props.joke && this.props.joke.setup}</p>
+                    <p className="p1">{this.props.joke && this.props.joke.setup}</p>
 
-                <p>{this.state.punchline ? this.props.joke.punchline : ''}</p>
-
+                    <p className="p2">{this.state.punchline ? this.props.joke.punchline : ''}</p>
+                </div>
             </div>
         )
     }
